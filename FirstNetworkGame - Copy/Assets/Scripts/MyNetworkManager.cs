3129cef14392529ca8e1cf8ab289955adceca8ad
Edd Brown <edd_brown@bloomfield.edu>
2015-12-09 12:32:40 -0500

    onstartclient

--- FirstNetworkGame - Copy/Assets/Scripts/MyNetworkManager.cs
+++ FirstNetworkGame - Copy/Assets/Scripts/MyNetworkManager.cs
@@ -7,7 +7,6 @@ public class MyNetworkManager : NetworkManager
 
 	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId) 
 	{
-		Color color = new Color(Random.Range(0.0f, 1.0f),Random.Range(0.0f, 1.0f),Random.Range(0.0f, 1.0f));
 		GameObject playerToSpawn = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
 		playerToSpawn.GetComponent<Player>().color = new Color(Random.Range(0.0f, 1.0f),Random.Range(0.0f, 1.0f),Random.Range(0.0f, 1.0f));
 		NetworkServer.AddPlayerForConnection(conn, playerToSpawn, playerControllerId);
--- FirstNetworkGame/Assets/Scripts/Player.cs
+++ FirstNetworkGame/Assets/Scripts/Player.cs
@@ -6,7 +6,10 @@ public class Player : NetworkBehaviour
 	[SyncVar]
 	public Color color;
 
-
+	public override void OnStartClient() 
+	{
+		gameObject.GetComponent<Renderer>().material.color = color;
+	}
 	// Use this for initialization
 	void Start () 
 	{
