0055d7c8d9d1ac85572e263015eb2dee69b61215
Edd Brown <edd_brown@bloomfield.edu>
2015-12-09 13:58:31 -0500

    kills >:)

--- FirstNetworkGame/Assets/Scripts/Bullet.cs
+++ FirstNetworkGame/Assets/Scripts/Bullet.cs
@@ -11,11 +11,11 @@ public class Bullet : NetworkBehaviour
 	{
 		gameObject.GetComponent<Renderer>().material.color = color;
 	}	
-	// Use this for initialization
-	void Start () {
-	
+
+	void OnTriggerEnter(Collider other)
+	{
+		Destroy(other.gameObject);
 	}
-	
 	// Update is called once per frame
 	void Update () {
 	
