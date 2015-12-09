7913c62275183508c1a3743b0479536e5eb019e9
Edd Brown <edd_brown@bloomfield.edu>
2015-12-09 12:45:29 -0500

    Remote-call
    Syncs!

--- FirstNetworkGame - Copy/Assets/Scripts/Player.cs
+++ FirstNetworkGame - Copy/Assets/Scripts/Player.cs
@@ -6,17 +6,47 @@ public class Player : NetworkBehaviour
 	[SyncVar]
 	public Color color;
 
-
+	float moveSpeed = 1.875f;
+	public override void OnStartClient() 
+	{
+		gameObject.GetComponent<Renderer>().material.color = color;
+	}
 	// Use this for initialization
 	void Start () 
 	{
 
 
 	}
+
+	void GetInput() 
+	{
+		float x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+		float y = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+		
+		if(isServer) 
+			RpcMoveIt(x,y);
+		 else 
+			CmdMoveIt(x,y);
+
+	}
 	
+	[ClientRpc]
+	void RpcMoveIt(float x, float y) 
+	{
+		transform.Translate(x,y,0);
+	}
+	
+	[Command]
+	public void CmdMoveIt(float x, float y) 
+	{
+		RpcMoveIt(x,y);
+	}
+
 	// Update is called once per frame
 	void Update () 
 	{
-
+		if(isLocalPlayer) 
+			GetInput();
+		
 	}
 }
