d45aa41c3d332707a4c40342469b409bd437f5a0
Edd Brown <edd_brown@bloomfield.edu>
2015-12-09 12:40:16 -0500

    Controller.localplayer

--- FirstNetworkGame/Assets/Scripts/Player.cs
+++ FirstNetworkGame/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : NetworkBehaviour
 	[SyncVar]
 	public Color color;
 
+	float moveSpeed = 1.875f;
 	public override void OnStartClient() 
 	{
 		gameObject.GetComponent<Renderer>().material.color = color;
@@ -16,10 +17,22 @@ public class Player : NetworkBehaviour
 
 
 	}
-	
+
+	void GetInput() 
+	{
+		float x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+		float y = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+		MoveIt(x,y);
+	}
+	void MoveIt(float x, float y) 
+	{
+		transform.Translate(x,y,0);
+	}
 	// Update is called once per frame
 	void Update () 
 	{
-
+		if(isLocalPlayer) 
+			GetInput();
+		
 	}
 }
