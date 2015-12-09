05f6a95c9f95e03768c2cbd47e92d290fab5b2d3
Edd Brown <edd_brown@bloomfield.edu>
2015-12-09 12:28:06 -0500

    step 3

---
+++ FirstNetworkGame - Copy/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+public class MyNetworkManager : NetworkManager 
+{
+
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId) 
+	{
+		Color color = new Color(Random.Range(0.0f, 1.0f),Random.Range(0.0f, 1.0f),Random.Range(0.0f, 1.0f));
+		GameObject playerToSpawn = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+		playerToSpawn.GetComponent<Player>().color = new Color(Random.Range(0.0f, 1.0f),Random.Range(0.0f, 1.0f),Random.Range(0.0f, 1.0f));
+		NetworkServer.AddPlayerForConnection(conn, playerToSpawn, playerControllerId);
+
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ FirstNetworkGame/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+using UnityEngine.Networking;
+public class Player : NetworkBehaviour 
+{
+	[SyncVar]
+	public Color color;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
